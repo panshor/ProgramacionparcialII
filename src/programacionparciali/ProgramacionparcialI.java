b79cf5997f41132b928c74c9034919407e7310a0
panshor <juanjodc79@gmail.com>
2025-10-29 18:02:11 -0500

    parcialprogramacionII

--- src/programacionparciali/ProgramacionparcialI.java
+++ src/programacionparciali/ProgramacionparcialI.java
@@ -4,8 +4,6 @@
  */
 package programacionparciali;
 
-import java.util.Scanner;
-
 /**
  *
  * @author juanj
@@ -16,24 +14,7 @@ public class ProgramacionparcialI {
      * @param args the command line arguments
      */
     public static void main(String[] args) {
-        Scanner leer = new Scanner(System.in);
-        System.out.print("Digite su divisa (dolar o peso): ");
-        String divisa = leer.nextLine();
-        if(divisa.equals("dolar")){
-            System.out.println("Digite los dólares: ");
-            int dolar = leer.nextInt();
-            int conversion = dolar*4700;
-            System.out.println("Su conversión es: "+conversion+" pesos");
-            
-        }
-        if(divisa.equals("peso")){
-            System.out.println("Digite los pesos: ");
-            int peso = leer.nextInt();
-            float conversionp = peso/4700;
-            System.out.println("Su conversión es: "+conversionp+" dolares");
-        }
-
-    }
+        // TODO code application logic here
     }
     
-
+}
